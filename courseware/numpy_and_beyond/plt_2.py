3428e94f6ec5b644a085c1a0e28f131f018a30bc
miaodx <miaodx@hotmail.com>
2017-05-04 14:23:29 +0800

    Chinese character in plot.

--- courseware/numpy_and_beyond/plt_2.py
+++ courseware/numpy_and_beyond/plt_2.py
@@ -1,5 +1,9 @@
+#-*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif']=['SimHei']
 
 # Compute the x and y coordinates for points on sine and cosine curves
 x = np.arange(0, 3 * np.pi, 0.1)
@@ -11,6 +15,6 @@ plt.plot(x, y_sin)
 plt.plot(x, y_cos)
 plt.xlabel('x axis label')
 plt.ylabel('y axis label')
-plt.title('Sine and Cosine')
+plt.title('Sine and Cosine 中文啊')
 plt.legend(['Sine', 'Cosine'])
 plt.show()
